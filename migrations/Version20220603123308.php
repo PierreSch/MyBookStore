aa5894591ecc002a82a44225e11ebe85a0d25b41
PierreSch <pierreschenberg@yahoo.fr>
2022-06-03 14:46:47 +0200

    fixtures

--- migrations/Version20220603123308.php
+++ migrations/Version20220603123308.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220603120202 extends AbstractMigration
+final class Version20220603123308 extends AbstractMigration
 {
     public function getDescription(): string
     {
---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    const DATA =[
+        // Category 1
+        [
+            'name' => "Roman",
+            'description' => null,
+            'color' => "#FF9900",
+            'illustration' => null,
+        ],
+        // Category 2
+        [
+            'name' => "Policier",
+            'description' => null,
+            'color' => "#0099FF",
+            'illustration' => null,
+        ],
+        // Category 3
+        [
+            'name' => "Nouvelle",
+            'description' => null,
+            'color' => "#CC00DD",
+            'illustration' => null,
+        ],
+    ];
+
+    public function load(ObjectManager $manager): void
+    {
+        foreach (self::DATA as $item)
+        {
+            $category = new Category;
+
+            $category->setName($item['name']);
+            $category->setDescription($item['description']);
+            $category->setColor($item['color']);
+            $category->setIllustration($item['illustration']);
+
+            $manager->persist($category);
+        }
+
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+}
